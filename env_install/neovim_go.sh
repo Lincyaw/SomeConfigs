3ef60d0dc36c81391e2e436683fcbc2448155144
lincyaw <814750204@qq.com>
2021-06-04 23:03:42 +0800

    update

--- env_install/neovim_go.sh
+++ env_install/neovim_go.sh
@@ -30,3 +30,12 @@ nvim ~/.config/nvim/init.vim
 # If the plugins didn't automatically install, press colon : then type PlugInstall and press Enter
 # Use vim-go to install binaries, press colon : then type GoInstallBinaries and press Enter
 
+# " Spaces & Tabs {{{ 
+# set tabstop=4       " number of visual spaces per TAB 
+# set softtabstop=4   " number of spaces in tab when editing
+# set shiftwidth=4    " number of spaces to use for autoindent 
+# set expandtab       " tabs are space 
+# set autoindent \ 
+# set copyindent      " copy indent from the previous line 
+# " }}} Spaces & Tabs 
+
